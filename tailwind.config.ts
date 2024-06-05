f835c2639d595231f03d30abe46d42352ac5be9a
Agustin Rojas <rojasagusf01@gmail.com>
2024-06-04 21:03:19 -0300

    Add configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        darkGeneral: "#0d0d0d",
+        accentColor: "#29cc8d"
+      }
     },
   },
   plugins: [],
