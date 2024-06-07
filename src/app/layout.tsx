7819879d9b9f3e934ae53fab8229d1153f0bb24e
Agustin Rojas <rojasagusf01@gmail.com>
2024-06-07 15:23:45 -0300

    Modify layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import localFont from 'next/font/local'
+import Header from "@/components/Header";
+import { ViewTransitions } from 'next-view-transitions'
 
 const delusionFont = localFont({ src: './../assets/fonts/delusiondisplay-regular.otf', variable: '--font-delusion' })
 
@@ -19,10 +21,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-darkGeneral scroll-smooth">
-      <body className={`${inter.className} ${delusionFont.variable} max-h-screen max-w-screen-xl p-6 mx-auto my-0 flex flex-col justify-start items-center`}>
-        {children}
-      </body>
-    </html>
+    <ViewTransitions>
+      <html lang="en" className="bg-darkGeneral scroll-smooth">
+        <body className={`${inter.className} ${delusionFont.variable} max-h-screen max-w-screen-xl p-6 mx-auto my-0 flex flex-col justify-start items-center overflow-x-hidden`}>
+          <Header />
+          {children}
+        </body>
+      </html>
+    </ViewTransitions>
   );
 }
