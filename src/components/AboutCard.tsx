64d219526324d78b881032a50106983952ef8649
Agustin Rojas <rojasagusf01@gmail.com>
2024-06-13 14:20:24 -0300

    Add about section card

---
+++ src/components/AboutCard.tsx
@@ -0,0 +1,8 @@
+export default function AboutCard({title, description} : {title: string, description: string}) {
+  return (
+    <div className="bg-darkGeneral min-h-96 w-full md:w-1/5 text-white rounded-lg p-6 flex flex-col justify-center items-center max-w-96 min-w-96">
+      <h3 className="font-bold tracking-widest text-2xl mb-6 text-center">{title}</h3>
+      <p className="text-slate-200 text-center">{description}</p>
+    </div>
+  )
+}
