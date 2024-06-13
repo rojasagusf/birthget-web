69eaaf52050f283db70cb547e51913a5dcc06e58
Agustin Rojas <rojasagusf01@gmail.com>
2024-06-13 14:21:26 -0300

    Add general button

--- src/components/GeneralButton.tsx
+++ src/components/GeneralButton.tsx
@@ -1,5 +1,7 @@
 'use client';
 import {motion} from 'framer-motion';
+import Image from 'next/image';
+import spinner from '@/assets/spinner.svg';
 
 const types = {
   primary: 'bg-accentColor duration-150',
@@ -8,15 +10,23 @@ const types = {
 
 type ButtonType = keyof typeof types;
 
-export default function GeneralButton({label, type, action}: {label: string, type: ButtonType, action?: () => void}) {
+export default function GeneralButton({label, type, action, loading}: {label: string, type: ButtonType, action?: () => void, loading?: boolean}) {
+
+  const checkIfLoading = () => {
+    if (loading) {
+      return <Image src={spinner} width={60} height={20} alt='Spinner' />;
+    }
+
+    return label;
+  }
+
   return (
     <motion.button
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }}
+      whileTap={{ scale: 0.5 }}
       className={`p-2 rounded-lg ${types[type]} w-full font-bold text-white min-w-28`}
       onClick={action}
      >
-      {label}
+      {checkIfLoading()}
     </motion.button>
   )
 }
